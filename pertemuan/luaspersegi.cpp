b33573d9fdcf1e7c56fd18f1fbc3e21f6a185770
root <root@LAPTOP-I3DUS8KC>
2023-04-04 04:51:11 +0100

    first

---
+++ pertemuan/luaspersegi.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int panjang;
+    int lebar;
+    cout << "Masukkan nilai panjang";
+    cin >> panjang;
+    cout << "Masukkan nilai lebar";
+    cin >> lebar;
+    int luas;
+    luas = panjang * lebar;
+    cout << luas;
+     
+}
